b9625f91e7f3b4a84ab3c3d43c22dff214610d5d
KarenGoncalves <cris.kgs@gmail.com>
2024-03-20 15:32:14 -0400

    gh-pages

---
+++ useful_R_commands/posts/styles.R
@@ -0,0 +1,14 @@
+cat("<style>
+code{
+	font-size:1.1em;
+	color:yellow;
+	background-color:#2b2b2b;
+	border-radius:.25rem;
+	padding:.125rem .25rem;
+	word-wrap:break-word
+}
+
+code span {font-size: 1.1em}
+code span.fu {color: yellow}
+
+</style>")
