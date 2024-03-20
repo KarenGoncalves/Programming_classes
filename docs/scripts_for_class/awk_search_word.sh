8dd4ad63d923ad7e643a8b4c368c75e712f33044
KarenGoncalves <cris.kgs@gmail.com>
2024-03-20 15:37:19 -0400

    Built site for gh-pages

--- docs/scripts_for_class/awk_search_word.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Print lines containing "#" in blastp_results.txt
-awk '/#/ {print}' blastp_results.txt
-
-# Print lines containing "#" in blastp_results.txt
-awk '!/#/ {print}' blastp_results.txt
